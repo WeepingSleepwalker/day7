ce9eaa67c0e22cd0527a2f3f1b27bd27edd24a97
Francis Chung <francis.chung@gmail.com>
2021-08-23 13:52:06 -0700

    fizz exercise

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+function fizzbuzz(){
+for (var i = 0; i < 101; i++){
+    console.log(i)
+    if (i%3  ==  0){
+        console.log("fizz");
+    }
+    if (i%5 == 0){
+        console.log("buzz");
+    }
+    }
+}
+fizzbuzz();
